58274006ddb889ce3e8dce35e673550c6f90430a
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-15 13:45:08 +0700

    Solve the 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...removeValues) {
+    for (const removeValue of removeValues){
+        for (let i = 0 ; i < array.length ; i++){
+            if (removeValue===array[i]){
+                array.splice(i,1)
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
