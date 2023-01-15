9888a6da2e904f9d90107b90b8d333cbe74b6ed8
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-15 14:02:45 +0700

    Solve the 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if ((firstNumber < 0) || (secondNumber < 0)) {
+        return "ERROR";
+    } else if ((typeof firstNumber != "number") || (typeof secondNumber != "number")) {
+        return "ERROR";
+    }
 
+    let sumNumber=0;
+    let beginNumber= (firstNumber<secondNumber) ? firstNumber : secondNumber;
+    let endNumber= (firstNumber<secondNumber) ? secondNumber : firstNumber;
+    for (let i=beginNumber;i<=endNumber;i++){
+        sumNumber+=i;
+    }
+    return sumNumber;
 };
 
 // Do not edit below this line
