f46faf4e626befe6dd519e6669652f4104b8bb34
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-18 21:13:48 +0700

    Solve the 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let totalSum=0;
+	for(let i=0;i<=num.length-1;i++){
+    totalSum+=num[i];
+  }
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let totalMultiply=num[0];
+  for(let i=1;i<=num.length-1;i++){
+    totalMultiply*=num[i];
+  }
+  return totalMultiply;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  for (let i=num-1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
